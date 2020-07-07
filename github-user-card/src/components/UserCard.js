4f1f7318baed816810d449d8d143baf263308f91
maribelcuales <maribelcuales@gmail.com>
2020-07-07 14:18:24 +0800

    Build out a user card displaying the return data from the Github API.

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import "./styles.css";
+
 
 function UserCard(props) {
   return (
-    <div>
-      <img src={props.githubUser.avatar_url} alt={props.githubUser.name}/>
-      <h3>Name: {props.githubUser.name}</h3>
-      <p>Login: {props.githubUser.login}</p>
-      <p>Bio: {props.githubUser.bio}</p>
-      <p>Github URL: {props.githubUser.url}</p>
+    <div className="card-image-container">
+      <img 
+        className="card-image"
+        top width="100%" 
+        src={props.githubUser.avatar_url} 
+        alt={props.githubUser.name} 
+      />
+      <div className="card-details-container">
+        <h3>{props.githubUser.name}</h3>
+        <p>{props.githubUser.login}</p>
+        <p>{props.githubUser.bio}</p>
+
+        <Nav>
+          <NavLink className="github-url" href={props.githubUser.html_url} target="_blank">
+            Github URL
+          </NavLink>
+        </Nav>
+      </div>
     </div>
   );
 }
