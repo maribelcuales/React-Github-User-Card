dd596a481bb03b5b711adaf0152181907f1887bc
maribelcuales <maribelcuales@gmail.com>
2020-07-07 11:17:11 +0800

    Passed data to UserCard component.

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function UserCard() {
+function UserCard(props) {
   return (
-    <h2>User Info</h2>
-
-  )
+    <div>
+      <img src={props.githubUser.avatar_url} alt={props.githubUser.name}/>
+      <h3>Name: {props.githubUser.name}</h3>
+      <p>Login: {props.githubUser.login}</p>
+      <p>Bio: {props.githubUser.bio}</p>
+      <p>Github URL: {props.githubUser.url}</p>
+    </div>
+  );
 }
 
 export default UserCard; 
